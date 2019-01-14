37d87c8b1c758ffa66f83206fafb99c144c5528b
n.fedorenko <n.fedorenko@pixelplex.io>
2019-01-14 14:36:41 +0300

    fixes

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-#!/bin/sh
-
-set -e
-
-git init
-bundle install
-cd app
-bundle exec pod install
-
 GREEN='\033[0;32m'
 echo -e "${GREEN}Project successfuly generated!"
 echo -e "${GREEN}You will need to install bundler and pods for the project. Checkout the generated project Readme to get started."
