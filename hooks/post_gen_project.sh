ccc1c054ac064370a3fe8dad537c8b935a1bdcbe
n.fedorenko <n.fedorenko@pixelplex.io>
2019-01-14 17:39:49 +0300

    pods added

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,4 +2,8 @@
 
 GREEN='\033[0;32m'
 echo -e "${GREEN}Project successfuly generated!"
-echo -e "${GREEN}You will need to install bundler and pods for the project. Checkout the generated project Readme to get started."
+
+bundle install
+bundle exec pod install
+
+echo -e "${GREEN}Pods successfuly installed!"
