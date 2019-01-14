388467036ace0845f2a14aabbde4de4ba87ea080
n.fedorenko <n.fedorenko@pixelplex.io>
2019-01-14 14:32:13 +0300

    initial app

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -9,8 +9,6 @@ bundle install
 cd app
 bundle exec pod install
 
-bundle exec synx --no-sort-by-name "{{ cookiecutter.project_name | replace(' ', '') }}.xcodeproj"
-
 GREEN='\033[0;32m'
 echo -e "${GREEN}Project successfuly generated!"
 echo -e "${GREEN}You will need to install bundler and pods for the project. Checkout the generated project Readme to get started."
---
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  {{cookiecutter.app_name}}
+//
+//  Created by Fedorenko Nikita on 1/14/19.
+//  Copyright © 2019 PixelPlex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
