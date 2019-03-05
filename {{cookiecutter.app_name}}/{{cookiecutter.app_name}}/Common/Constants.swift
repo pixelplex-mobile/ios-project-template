cd574a138f400a73d071be4ddca477a38bb2de60
n.fedorenko <n.fedorenko@pixelplex.io>
2019-03-05 15:27:44 +0300

    year of creation added as variable

--- {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/Common/Constants.swift
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/Common/Constants.swift
@@ -3,7 +3,7 @@
 //  {{cookiecutter.app_name}}
 //
 //  Created by {{cookiecutter.lead_dev}} on {% now 'local', '%d.%m.%Y' %}.
-//  Copyright © 2018 {{cookiecutter.company_name}}. All rights reserved.
+//  Copyright © {% now 'local', '%Y' %} {{cookiecutter.company_name}}. All rights reserved.
 //
 
 import Foundation
